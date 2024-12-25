0dfd4749e23c2abe541d1253cd7394b758f47e37
yunxi <yunxi177@gmail.com>
2024-12-25 09:37:56 +0800

    解决 visual mutil 使用后，无法进行回车补全代码问题

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -191,6 +191,22 @@ return {
 	},
 	{
 		"mg979/vim-visual-multi",
+		init = function()
+			vim.g.VM_theme = "purplegray"
+			vim.g.VM_mouse_mappings = 1
+			-- vim.schedule(function()
+			vim.g.VM_maps = {
+				["I BS"] = "",
+				["Goto Next"] = "]v",
+				["Goto Prev"] = "[v",
+				["I CtrlB"] = "<M-b>",
+				["I CtrlF"] = "<M-f>",
+				["I Return"] = "<S-CR>",
+				["I Down Arrow"] = "",
+				["I Up Arrow"] = "",
+			}
+			-- end)
+		end,
 	},
 	{
 		"Exafunction/codeium.nvim",
